3c5d7dc52d0ed7492be6fa74fbf2c5ce45d55e03
zhxke <zhxke@bainainfo.com>
2014-08-07 16:52:23 +0800

    add the user relevant information and add the heart beat to check the server exists

--- src/main/SimpleChatClient.java
+++ src/main/SimpleChatClient.java
@@ -3,15 +3,19 @@ package main;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
-
+import java.net.UnknownHostException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -20,41 +24,78 @@ import javax.swing.ScrollPaneConstants;
 
 public class SimpleChatClient {
     JTextArea incoming;
+    // JEditorPane incoming;
     JTextField outgoing;
     BufferedReader reader;
     PrintWriter writer;
     Socket socket;
+    String address;
+    String user_name;
+
+    public SimpleChatClient() {
+        try {
+            InetAddress addr = InetAddress.getLocalHost();
+            address = addr.getHostAddress();
+            user_name = addr.getHostName();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String args[]) {
         SimpleChatClient client = new SimpleChatClient();
+
         client.go();
     }
 
     public void go() {
-        JFrame frame = new JFrame("SimpleChat");
+        JFrame frame = new JFrame(user_name + "'s SimpleChat");
         JPanel mainPanel = new JPanel();
         incoming = new JTextArea(15, 50);
         incoming.setLineWrap(true);
         incoming.setWrapStyleWord(true);
+        // incoming=new JEditorPane();
         incoming.setEnabled(false);
         JScrollPane qScroller = new JScrollPane(incoming);
         qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         outgoing = new JTextField(20);
         JButton sendButton = new JButton("send");
-         sendButton.addActionListener(new SendButtonListener());
+        sendButton.addActionListener(new SendButtonListener());
         mainPanel.add(qScroller);
         mainPanel.add(outgoing);
         mainPanel.add(sendButton);
 
         setUpNetworking();
 
-        Thread readerThread=new Thread(new IncomingReader());
+        Thread readerThread = new Thread(new IncomingReader());
         readerThread.start();
 
+        Thread serverExist = new Thread(new CheckServer());
+        serverExist.start();
+
         frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        frame.setSize(400, 500);
+        frame.setSize(600, 320);
         frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                int value = JOptionPane.showConfirmDialog(null, "确定要关闭吗？");
+                if (value == JOptionPane.OK_OPTION) {
+                    writer.println(user_name + "quit the chat");
+                    writer.flush();
+                    try {
+                        socket.close();
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+
+                    System.exit(0);
+                }
+            }
+
+        });
     }
 
     public void setUpNetworking() {
@@ -64,47 +105,86 @@ public class SimpleChatClient {
             reader = new BufferedReader(streamReader);
             writer = new PrintWriter(socket.getOutputStream());
             System.out.println("networking established");
+            writer.println(address + "join the chat");
+            writer.flush();
         } catch (IOException e) {
             // TODO Auto-generated catch block
+            incoming.append("sorry, can not connect the server!");
             e.printStackTrace();
         }
 
     }
 
-    public class SendButtonListener implements ActionListener { //in class (neibulei)
+    public class SendButtonListener implements ActionListener { // in class (neibulei)
 
         @Override
         public void actionPerformed(ActionEvent arg0) {
-            // TODO Auto-generated method stub
+            // TODO Auto-generated method stubadasd
             try {
-                writer.println(outgoing.getText());
+                if ((outgoing.getText() != "") && (outgoing.getText() != null))
+                    writer.println(address + ":");
+                writer.println("     " + outgoing.getText());
                 writer.flush();
             } catch (Exception e) {
                 e.printStackTrace();
 
+            }
+            try {
+                socket.sendUrgentData(0xFF);
+            } catch (Exception ex) {
+                incoming.append("the sever maybe occured some problems");  //click the send can make this warning
             }
             outgoing.setText("");
             outgoing.requestFocus();
         }
 
     }
-    
-    public class IncomingReader implements Runnable{
-        public void run()
-        {
+
+    public class IncomingReader implements Runnable {
+        public void run() {
             String message;
             try {
-                while((message=reader.readLine())!=null)
-                {
-                    incoming.append(message+"\n");
+                while ((message = reader.readLine()) != null) {
+                    incoming.append(message + "\n");
                 }
             } catch (Exception e) {
                 // TODO: handle exception
-                e.printStackTrace();
+
+                // e.printStackTrace();
             }
         }
     }
-    
-    
-    
+
+    public class CheckServer implements Runnable {
+
+
+        public void run() {
+            // TODO Auto-generated method stub
+            try {
+                Socket testSocket = new Socket("localhost", 5000);
+                while (true) {
+                    testSocket.sendUrgentData(0xFF);
+                    Thread.sleep(2000);
+                }
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                incoming.append("the sever maybe occured some problems,can't disconnect");
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                incoming.append("the sever maybe occured some problems,can't disconnect");
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+    }
+
+
+
 }
--- src/main/SimpleChatServer.java
+++ src/main/SimpleChatServer.java
@@ -5,82 +5,86 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 
 
 public class SimpleChatServer {
-    ArrayList clientOutputStreams;
-    
-    public static void main(String[] args)
-    {
+    ArrayList<PrintWriter> clientOutputStreams;
+
+    public static void main(String[] args) {
         new SimpleChatServer().go();
-        
+
     }
-    
-    public class Clienthandler implements Runnable{
+
+    public class Clienthandler implements Runnable {
         BufferedReader reader;
         Socket socket;
-        
-        
-        public Clienthandler(Socket clientSocket)
-        {
+
+
+        public Clienthandler(Socket clientSocket) {
             try {
-                socket=clientSocket;
-                InputStreamReader isReader=new InputStreamReader(socket.getInputStream());
-                reader=new BufferedReader(isReader);
+                socket = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+                reader = new BufferedReader(isReader);
             } catch (Exception e) {
                 // TODO: handle exception
                 e.printStackTrace();
             }
         }
-        
-       
+
+
         @Override
         public void run() {
             // TODO Auto-generated method stub
             String message;
             try {
-                while((message=reader.readLine())!=null)
-                {
+                while ((message = reader.readLine()) != null) {
                     tellEveryone(message);
                 }
             } catch (Exception e) {
                 // TODO: handle exception
+
             }
-            
+
         }
-        
+
     }
-    
-    public void go()
-    {
-        clientOutputStreams=new ArrayList();
+
+    public void go() {
+        clientOutputStreams = new ArrayList<PrintWriter>();
         try {
-            ServerSocket serverSocket=new ServerSocket(5000);
-            while(true){
-                Socket clientSocket=serverSocket.accept();
-                PrintWriter writer=new PrintWriter(clientSocket.getOutputStream());
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
                 clientOutputStreams.add(writer);
-                
+
                 Thread thread = new Thread(new Clienthandler(clientSocket));
                 thread.start();
                 System.out.println("got a connection");
+
             }
         } catch (Exception e) {
+
             // TODO: handle exception
+            e.printStackTrace();
         }
     }
-    
-    public void tellEveryone(String message)
-    {
-       Iterator users=clientOutputStreams.iterator();
-        while(users.hasNext())
-        {
-            PrintWriter writer=(PrintWriter)users.next();
+
+    public void tellEveryone(String message) {
+        Iterator<PrintWriter> users = clientOutputStreams.iterator();
+        while (users.hasNext()) {
+            try{
+            PrintWriter writer = (PrintWriter) users.next();
             writer.println(message);
             writer.flush();
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }
         }
     }
 
